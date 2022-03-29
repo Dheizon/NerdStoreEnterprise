7d0a3fbdd293748d1944191442249437a7b7c681
Dheizon Gonçalves <dheizonx@gmail.com>
2022-03-29 16:48:34 -0300

    Boas praticas com HttpServices

---
+++ src/web/NSE.WebApp.MVC/Extesions/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace NSE.WebApp.MVC.Extesions
+{
+    public class AppSettings
+    {
+        public string AutenticacaoUrl { get; set; }
+        public string CatalogoUrl { get; set; }
+    }
+}
