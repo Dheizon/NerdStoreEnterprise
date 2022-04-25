583eaa23994048b380e2e12ca54f43ba24afbe5c
Dheizon Gonçalves <dheizonx@gmail.com>
2022-04-25 14:22:59 -0300

    Implementação API de Catálogo

---
+++ src/services/NSE.Catalogo.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace NSE.Catalogo.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Nerd Store Enterprise Catálogo API",
+                    Description = "Esta API faz parte do curso ASP.NET Core Enterprise Applications",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact() { Name = "Dheizon Gonçalves", Email = "dheizon@motivetech.com.br" },
+                    License = new Microsoft.OpenApi.Models.OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
+        }
+
+        public static void UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+        }
+    }    
+}
