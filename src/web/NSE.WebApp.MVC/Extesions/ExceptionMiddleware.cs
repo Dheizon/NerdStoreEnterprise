bc5014e29668edd32abf69dc971e84e6b7a0cfd1
Dheizon Gonçalves <dheizonx@gmail.com>
2022-07-27 11:32:29 -0300

    Implementação Refit

--- src/web/NSE.WebApp.MVC/Extesions/ExceptionMiddleware.cs
+++ src/web/NSE.WebApp.MVC/Extesions/ExceptionMiddleware.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using NSE.WebApp.MVC.Services;
+using Refit;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Extesions
@@ -21,20 +23,26 @@ namespace NSE.WebApp.MVC.Extesions
             }
             catch (CustomHttpRequestException ex)
             {
-                HandleRequestExceptionAsync(httpContext, ex);
+                HandleRequestExceptionAsync(httpContext, ex.StatusCode);
+            } catch (ValidationApiException ex)
+            {
+                HandleRequestExceptionAsync(httpContext, ex.StatusCode);
+            } catch  (ApiException ex)
+            {
+                HandleRequestExceptionAsync(httpContext, ex.StatusCode);
             }
 
         }
 
-        private static void HandleRequestExceptionAsync(HttpContext httpContext, CustomHttpRequestException httpRequestException)
+        private static void HandleRequestExceptionAsync(HttpContext httpContext, HttpStatusCode statusCode)
         {
-            if (httpRequestException.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            if (statusCode == HttpStatusCode.Unauthorized)
             {
                 httpContext.Response.Redirect($"/login?ReturnUrl{httpContext.Request.Path}");
                 return;
             }
             
-            httpContext.Response.StatusCode = (int)httpRequestException.StatusCode;
+            httpContext.Response.StatusCode = (int)statusCode;
         }
     }
 }
